4379a743cc6443d8f1e149e411b312f832f0f3f6
Damien7200 <damienwaldon@gmail.com>
2025-06-25 22:55:05 +1000

    Text Input Created (not complete)
    I created a input box using the text area element, i then layed this
    element over some premade transperent texts which was sent by the
    practise view. The user can then type over the transperent text, using
    it as a guidline. I also created a javescript function that disables the
    enter key as this was allowing ther user to missalighn the cursor.

--- Typing_Tutor/Typing/views.py
+++ Typing_Tutor/Typing/views.py
@@ -5,7 +5,16 @@ def home(request):
     return render(request, "Typing/home.html")
 
 def Practise(request):
-    return render(request, "Typing/practise.html")
+    words = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    if request.method == "POST":
+        pass
+    
+
+    return render(request, "Typing/practise.html", {'Words' : words} )
 
 def Wordbank(request):
     return render (request, "Typing/Word_bank.html")
+
+
+
+
